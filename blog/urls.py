4540865de8e7ccc2400d4ac8991551e33609e060
Patrick Hanley <49796873+patrickhanl@users.noreply.github.com>
2019-05-13 07:41:49 -0400

    added create/edit views and minor security

--- blog/urls.py
+++ blog/urls.py
@@ -19,4 +19,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit.html', views.post_edit, name='post_edit'),
 ]
