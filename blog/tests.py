36437d628b856524a2011cb08a3e0f6cf614a8f3
ShreehariVaasishta <shreehari9481@gmail.com>
2021-06-12 17:11:49 +0530

    test 1

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
 # Create your tests here.
+class GithubActionsTests(TestCase):
+    def run_actions(self):
+        assert 200 == 200
--- blog/urls.py
+++ blog/urls.py
@@ -21,16 +21,14 @@ from django.conf.urls import url
 
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    url(r'^tracking/', include('tracking.urls')),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    path('post/post_edit/', views.post_edit, name='post_edit'),
-    path('post/new/', views.post_new, name='post_new'),
-    path('post/<int:pk>/edit.html', views.post_edit, name='post_edit'),
+    path("", views.post_list, name="post_list"),
+    path("post/<int:pk>/", views.post_detail, name="post_detail"),
+    path("post/post_edit/", views.post_edit, name="post_edit"),
+    path("post/new/", views.post_new, name="post_new"),
+    path("post/<int:pk>/edit.html", views.post_edit, name="post_edit"),
     path("register/", views.register, name="register"),
     path("login/", views.login_request, name="login"),
     url(r"^logout/$", views.logout_request, name="logout"),
-
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
